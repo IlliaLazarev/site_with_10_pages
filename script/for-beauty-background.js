44271fb24c4f925c05d5e7f01921a07d052f513c
IlliaLazarev <lazarevillia2008@gmail.com>
2025-11-23 12:51:40 +0200

    commit by illiaLazarev

---
+++ script/for-beauty-background.js
@@ -0,0 +1,7 @@
+const bg = document.querySelector('.water-bg');
+
+document.addEventListener('mousemove', e => {
+  const x = (e.clientX / window.innerWidth) * 100;
+  const y = (e.clientY / window.innerHeight) * 100;
+  bg.style.background = `radial-gradient(circle at ${x}% ${y}%, #ebe98e, #f8bc84, #f3a45b)`;
+});
